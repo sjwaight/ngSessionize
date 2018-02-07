1b90540b2c77bff9ce4f52967a57e624ac8da321
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2018-02-07 15:03:24 -0500

    Initial commit

--- components/sessionizeSessionsFilter.component.js
+++ components/sessionizeSessionsFilter.component.js
@@ -17,11 +17,39 @@ angular.module("ecs")
 
 		  vm.initialized = false;
 
+
+		  vm.createList = function(type) {
+
+		  	var list = [];
+
+		  	for(var i=0; i < vm.data.sessions.length; i++) {
+		  		var session = vm.data.sessions[i];
+
+				var thisCategory = $filter("filter")(session.categories, { name: type });
+
+				for(var j=0; j < thisCategory.categoryItems.length; j++) {
+
+					var thisItem = thisCategory.categoryItems[j];
+
+					list.push(thisItem)
+
+				}
+
+		    }
+
+		    return list;
+
+		  };
+
 		  vm.$onInit = function () {
 
 			  vm.statusMessage = "Loading sessions...";
 
 			  ecsService.initEcs().then(function () {
+
+				  vm.formats = createList("Session format");
+				  vm.tracks = createList("Track");
+				  vm.tags = createList("Tags");
 
 				  vm.initialized = true;
 
