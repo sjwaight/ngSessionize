50260a17d04a901f8bf0382f87762fbeba46e6f6
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2018-02-09 21:02:47 -0500

    Initial commit

--- components/sessionizeSessionsFilter.component.js
+++ components/sessionizeSessionsFilter.component.js
@@ -78,7 +78,7 @@ angular.module("ecs")
 			  // Count 'em
 			  for (var i = 0; i < vm.filteredSessions.length; i++) {
 
-				  var session = vm.data.filteredSessions[i];
+				  var session = vm.filteredSessions[i];
 
 				  for (var f in vm.filters) {
 					  if (vm.filters.hasOwnProperty(f)) {
